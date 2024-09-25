97dab19327327679bbbf3e710536ccd2527e583d
Trung Nam <trungnam.work@gmail.com>
2024-09-25 11:45:35 +0700

    feat(setup): Creating Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
-export default function App() {
+import React from 'react'
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from './sections'
+
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      {/* <Nav /> */}
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+
+      <section className="padding">
+        <PopularProducts />
+      </section>
+
+      <section className="padding">
+        <SuperQuality />
+      </section>
+
+      <section className="padding padding-x py-10">
+        <Services />
+      </section>
+
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
   )
 }
+
+export default App
