d464651ff6b24d42bc9a2e05721275b473239ecc
Trung Nam <trungnam.work@gmail.com>
2024-09-26 00:57:18 +0700

    feat(ui): Super Quality Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,11 +10,12 @@ const Button = ({ label, iconUrl }) => {
         border-coral-red"
         >
             {label}
-            <img
+
+            {iconUrl && <img
                 src={iconUrl}
                 alt="arrow right icon"
                 className='ml-2 rounded-full w-5 h-5'
-            />
+            />}
         </button>
     )
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
-
+import Button from '../components/Button'
+import { shoe8 } from "../assets/images";
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section
+      id='about-us'
+      className='flex justify-center
+      items-center max-lg:flex-col
+      gap-10 w-full max-container'
+    >
+      <div
+        className='flex flex-1
+        flex-col'
+      >
+        <h2 className='font-palanquin 
+          text-4xl capitalize font-bold
+          lg:max-w-lg'>
+          We Provide You
+          <span className='text-coral-red'> Supper</span>
+          <span className='text-coral-red'> Quality</span> Shoes
+        </h2>
+
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance..
+        </p>
+
+        <p className='mt-6 lg:max-w-lg info-text'>Our dedication to detail and excellence ensures your satisfaction</p>
+
+        <div className='mt-11'>
+          <Button label="View details" />
+        </div>
+      </div>
+
+      <div className='flex flex-1
+      justify-center items-center'>
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+        />
+      </div>
+    </section>
   )
 }
 
