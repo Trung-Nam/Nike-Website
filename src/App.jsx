5f835633c8ad062a267d9308423a5fdbb0415cae
Trung Nam <trungnam.work@gmail.com>
2024-09-25 19:06:25 +0700

    feat(ui):NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import {
   Subscribe,
   Footer,
 } from './sections'
-
+import Nav from './components/Nav'
 
 const App = () => {
   return (
     <main className="relative">
-      {/* <Nav /> */}
+      <Nav />
       <section className="xl:padding-1 wide:padding-r padding-b">
         <Hero />
       </section>
